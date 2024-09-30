ff0f87cfca82ce9c489e3892affacda8b9e5f076
emad-desoky <deskoemad@gmail.com>
2024-09-30 03:35:49 +0300

    ..

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* You can also add other sizes or formats if needed */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
