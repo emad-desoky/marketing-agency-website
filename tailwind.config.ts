6b18ec089087cde9ad58a00c9cb14eb921dc1693
emad-desoky <deskoemad@gmail.com>
2024-10-13 12:57:04 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config, PluginAPI } from "tailwindcss/types/config";
 import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 import tailwindcssAnimate from "tailwindcss-animate"; // Replace require with import
@@ -12,10 +13,8 @@ const addVariablesForColors = ({ addBase, theme }: PluginAPI) => {
     ":root": newVars,
   });
 };
-const svgToDataUri = require("mini-svg-data-uri");
-const colors = require("tailwindcss/colors");
+import svgToDataUri from "mini-svg-data-uri";
 const {
-  default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 
@@ -73,19 +72,23 @@ const config: Config = {
   plugins: [
     tailwindcssAnimate, // Replaced require with imported variable
     addVariablesForColors,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
           "bg-grid": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
           "bg-grid-small": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
           "bg-dot": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
