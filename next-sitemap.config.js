185520d06bf40516fde73e3b58b3a9871d422f73
emad-desoky <deskoemad@gmail.com>
2024-10-13 14:48:57 +0300

    seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
+// next-sitemap.js
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://tactics-digital-marketing.vercel.app/",
+  siteUrl: "https://tactics-digital-agency.vercel.app/",
   generateRobotsTxt: true, // (optional)
 };
 
