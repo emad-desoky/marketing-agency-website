ddaa4cbb4c22143441b16a3a87bb5cd75a0e3e29
emad-desoky <deskoemad@gmail.com>
2024-10-13 14:01:41 +0300

    ..

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: "https://tactics-digital-marketing.vercel.app/",
-  generateRobotsTxt: true, // (optional) Generate robots.txt file
-  // Other options can be added here
+  generateRobotsTxt: true, // (optional)
 };
 
 export default config;
