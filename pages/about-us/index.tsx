84fcab4e285cb7fcc2c28185ae432a9863accf2f
emad-desoky <deskoemad@gmail.com>
2024-10-14 16:41:15 +0300

    Update index.tsx

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,6 +1,5 @@
 import { Slider } from "@/components/about-us-data/Slider";
 import { StickyScrollRevealDemo } from "@/components/about-us-data/StickyScrollRevealDemo";
-import { TracingBeamDemo } from "@/components/about-us-data/TracingBeamDemo";
 import GetInTouch from "@/components/contact-us/GetInTouch";
 import Navbar from "@/components/navbar/Navbar";
 import React from "react";
