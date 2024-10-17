4053c829ce074cd0f07d5004d2af883634f329b9
emad-desoky <deskoemad@gmail.com>
2024-10-17 19:15:59 +0300

    Update [blogId].tsx

--- pages/blogs/[blogId].tsx
+++ pages/blogs/[blogId].tsx
@@ -1,8 +1,8 @@
 // components/Blog.tsx
+
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Head from "next/head";
 
 interface BlogData {
   id: string;
@@ -35,56 +35,11 @@ const Blog = () => {
 
   return (
     <>
-      {blog && (
-        <Head>
-          <title>{blog.title}</title>
-          <meta name="description" content={blog.description} />
-
-          {/* Open Graph Meta Tags */}
-          <meta property="og:title" content={blog.title} />
-          <meta property="og:description" content={blog.description} />
-          <meta
-            property="og:image"
-            content={
-              blog.images && blog.images.length > 0
-                ? blog.images[0]
-                : "/default-image.jpg"
-            }
-          />
-          <meta
-            property="og:url"
-            content={`https://tactics-digital-agency.vercel.app/blogs/${blogId}`}
-          />
-          <meta property="og:type" content="article" />
-          <meta property="og:site_name" content="Your Website Name" />
-
-          {/* Twitter Meta Tags */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={blog.title} />
-          <meta name="twitter:description" content={blog.description} />
-          <meta
-            name="twitter:image"
-            content={
-              blog.images && blog.images.length > 0
-                ? blog.images[0]
-                : "/default-image.jpg"
-            }
-          />
-          <meta name="twitter:site" content="@yourtwitterhandle" />
-
-          {/* Canonical URL */}
-          <link
-            rel="canonical"
-            href={`https://yourwebsite.com/blogs/${blogId}`}
-          />
-        </Head>
-      )}
-
       {blog ? (
         <div className="bg-black text-green-600 shadow-lg min-h-dvh max-h-auto">
           {/* Admin name and date */}
           <p className="text-gray-400 text-sm">
-            {blog.adminName} | {blog.date}
+            {blog.adminName} | {blog.date} {/* Display admin name and date */}
           </p>
           {/* Title */}
           <h1 className="text-4xl font-extrabold border-b-4 border-[rgb(255,228,0)] pb-2 mb-6">
@@ -95,7 +50,6 @@ const Blog = () => {
             <strong className="text-gray-200">Description:</strong>{" "}
             {blog.description}
           </p>
-          {/* Content */}
           <div
             dangerouslySetInnerHTML={{ __html: blog.content }}
             className="text-lg leading-relaxed"
