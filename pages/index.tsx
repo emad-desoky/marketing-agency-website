87985a6f582de13424f4a3f1c54c3e3d8418aa56
emad-desoky <deskoemad@gmail.com>
2024-10-18 00:30:40 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,40 +15,31 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Tactics Digital</title> {/* Set the page title */}
-        <meta
-          name="description"
-          content="WELCOME TO OUR DIGITAL AGENCY WE"
-        />{" "}
-        {/* Set the page description */}
-        <meta property="og:title" content="TACTICS DIGITAL AGENCY." />{" "}
-        {/* Open Graph title */}
+        <title>Tactics Digital</title>
+        <meta name="description" content="WELCOME TO OUR DIGITAL AGENCY WE" />
+        <meta property="og:title" content="TACTICS DIGITAL AGENCY." />
         <meta
           property="og:description"
           content="A brief description of your page"
-        />{" "}
-        {/* Open Graph description */}
+        />
         <meta
           property="og:image"
           content="https://plus.unsplash.com/premium_photo-1727279146711-c5be10449b70?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />{" "}
-        {/* Open Graph image */}
-        <meta property="og:url" content="https://yourwebsite.com" />{" "}
-        {/* Open Graph URL */}
-        <meta name="twitter:card" content="summary_large_image" />{" "}
-        {/* Twitter card type */}
-        <meta name="twitter:title" content="Your Page Title" />{" "}
-        {/* Twitter title */}
+        />
+        <meta
+          property="og:url"
+          content="https://marketing-agency-website-mu.vercel.app/"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Your Page Title" />
         <meta
           name="twitter:description"
           content="A brief description of your page."
-        />{" "}
-        {/* Twitter description */}
+        />
         <meta
           name="twitter:image"
           content="https://plus.unsplash.com/premium_photo-1727279146711-c5be10449b70?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />{" "}
-        {/* Twitter image */}
+        />
       </Head>
 
       <Navbar />
