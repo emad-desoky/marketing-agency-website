9a2d4eaa2922630ccdcffdf02a2c5eade06804e5
emad-desoky <deskoemad@gmail.com>
2024-10-17 16:52:18 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // Keep this line for static export
   images: {
+    unoptimized: true, // Disable image optimization
     domains: [
       "assets.aceternity.com",
       "cloudinary.hbs.edu",
