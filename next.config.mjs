d8d7c7413175ad381fe1b93cd8e30bdeb830de31
emad-desoky <deskoemad@gmail.com>
2024-10-17 16:49:55 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // Add this line for static export
   images: {
     domains: [
       "assets.aceternity.com",
